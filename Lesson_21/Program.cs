f38c1fea098e9f1dced228bfb5a404e9e664775b
Anna <110918574+AnnaSilver-Smith@users.noreply.github.com>
2022-09-02 23:22:25 +0500

    Update Program.cs

--- Lesson_21/Program.cs
+++ Lesson_21/Program.cs
@@ -18,7 +18,7 @@ double Distance(double x1, double x2,
                 double z1, double z2)
     {
         return Math.Sqrt(Math.Pow((x2 - x1), 2) + 
-                         Math.Pow((y2 -y1), 2) + 
+                         Math.Pow((y2 - y1), 2) + 
                          Math.Pow((z2 - z1), 2));
     }
 double lenght = Math.Round(Distance(x1, x2, y1, y2, z1, z2), 2 );
