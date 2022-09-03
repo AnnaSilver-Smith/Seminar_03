66c4e5c43c510d1252be082526386e9d843067b9
Anita <zorrodeplata.anita9@gmail.com>
2022-09-03 13:02:46 +0500

    Lesson 23

---
+++ Lesson_23/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Введите число: ");
+double N = double.Parse(Console.ReadLine());
+Console.Write($"{N} -> ");
+for(double i = 1; i <= N; i++)
+{
+    Console.Write($"{Math.Pow(i, 3)}{(i != N ? "," : "\n")}");
+}
